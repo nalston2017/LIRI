42b14f2c1b3f551056cef04e455c3e44032b533f
Nathan <nathanalston@gmail.com>
2018-02-06 23:42:13 -0500

    Twitter Function Added

--- liri.js
+++ liri.js
@@ -8,26 +8,64 @@ var twitQuire = ("twitter");
 var spotiFire = ("node-spotify-api");
 
 var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-var ombd = new OMDB(keys.omdb);
+var twitter = new Twitter(keys.twitter);
+var ombd = keys.omdb.apikey;
 
-// What LIRI should be able to switch from
+// The switch and cases for what to do based on the arguement
+switch (arugement) {
+  case "my-tweets": twitterAPI(); break;
+  case "spotify-this-song": spotifyAPI(); break;
+  case "movie-this": "OMDB function"; break;
+  case "do-what-it-says": "random function"; break;
 
-* `my-tweets`
+  console.log(
+    "\r\n" +
+    "LIRI is here to respond to whatever request you have. As long as it has to do with twitter, spotify, or OMBD" +
+    "\r\n" +
+    "First type node liri.js then you have the choice between the commands:" +
+    "\r\n\n"
+    " - my-tweets: + a twitter username to call the last 20 tweets of that user" +
+    "\r\n\n" +
+    " - spotify-this-song: + a song title to return song title and info that meet your request" +
+    "\r\n\n" +
+    " - movie-this: + a movie title will bull in information for that movie." +
+    "\r\n\n" +
+    " - do-what-it-says: will call the song 'I Want It That Way' by the Backstreet Boys." +
+    "\r\n\n" +
+    "The only catch is to put all requests in quotation marks (" ") or else I won't be able to understand you."
+  );
+}
 
-* `spotify-this-song`
+////////////////
+////Functions//
+///////////////
 
-* `movie-this`
+function twitterAPI() {
+  var userName = process.argv[3];
+  if(!userName) {
+    userName = "FiftyShades";
+  }
+  parameters = {
+    screen_name: userName
+  };
+  twitQuire.get("statuses/user_timeline/", parameters, function (
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      console.log(`Here are ${userName}'s latest tweets:\n`);
+      for (let i = 0; i < 20; i++) {
+        var tweetSponses =
+        "================================ \n\n" +
+        tweetSponses[i].text + "\n";
+        console.log(tweetSponses);
+        logdata(tweetSponses);
+      }
+    } else console.log("Sorry I couldn't fullfil your resquest. Please try again.")
+  });
+}
 
-* `do-what-it-says`
+function spotifyAPI() {
 
-// What it should be able to do
-// node liri.js my-tweets
-// This will show your last 20 tweets and when they were created at in your terminal/bash window
-
-// node liri.js spotify-this-song '<song name here>'
-// This will show the following information about the song in your terminal/bash window
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
+}
